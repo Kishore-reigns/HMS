322a4a8465c4abcb1b94fd674882404231621746
Kishore-reigns <112459807+Kishore-reigns@users.noreply.github.com>
2025-04-07 17:57:24 +0530

    added navbar

---
+++ src/components/Header_Footer/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { AppBar, Toolbar , Typography,Button} from '@mui/material'
+
+const Nav = () => {
+  return (
+    <AppBar position='staitc'>
+        <Toolbar>
+            <Typography variant='h5' component='div' sx={{flexGrow:1}}>KR HMS</Typography>
+
+            <Button color = "inherit">Login</Button>
+            <Button color = "inherit">Register</Button>
+            <Button color = "inherit">About</Button>
+            
+        </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Nav
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Nav from '../components/Header_Footer/Nav'
+
+const HomePage = () => {
+  return (
+    <Nav/>
+  )
+}
+
+export default HomePage
